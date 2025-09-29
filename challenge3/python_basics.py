0e94cc5fe91c4c38c0b806f6ce4000056543229c
Daveraj <daveraj.nothay@sky.uk>
2025-09-29 14:32:52 +0100

    CREATE: Added python_basics.py

---
+++ challenge3/python_basics.py
@@ -0,0 +1,38 @@
+class Human:
+    def __init__(self, first_name, last_name, is_alive, belongings, age, height, weight, coding_languages):
+        self.name = (first_name, last_name)
+        self.is_alive = is_alive
+        self.belongings = belongings
+        self.measurements = {
+            'age': age,
+            'height': height,
+            'weight': weight,
+        }
+        self.coding_languages = coding_languages
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.measurements['age']
+
+    def get_height(self):
+        return self.measurements['height']
+
+    def get_weight(self):
+        return self.measurements['weight']
+
+    def get_coding_languages(self):
+        return self.coding_languages
+
+    def get_belongings(self):
+        return self.belongings
+
+Human = Human("Panya", "Ranya", True, ['matcha'], 65, 376,
+              4000.1, {'Python', 'Java', 'C#', 'React', 'Assembly', 'C', 'C++',
+                                 'Rust', 'JavaScript', 'CSS', 'HTML', 'AWS', 'Lua', 'Haskell', 'LOLCODE',
+                                 'reMorse', 'TrumpScript' } )
+
+print(Human.get_name()[0])
+print(Human.get_age())
+
